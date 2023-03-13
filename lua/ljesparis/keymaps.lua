869cd3e2772115cdf630860a2851b0407fd918b9
Leonardo Esparis <ljesparis@gmail.com>
2023-03-13 22:28:00 +0100

    update keymaps
    * updated keymaps to move block of code
    * updated keymaps to move between buffers

--- lua/ljesparis/keymaps.lua
+++ lua/ljesparis/keymaps.lua
@@ -1,8 +1,8 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Move code down and up respectively
-vim.keymap.set("v", "<A-,>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "<A-.>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "T", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "B", ":m '<-2<CR>gv=gv")
 
 -- split vertically and split horizontally
 -- navigate and close.
@@ -15,13 +15,6 @@ vim.keymap.set("n", "mtb", "<C-w>j<CR>")
 vim.keymap.set("n", "<leader>1", "<C-w>o<CR>")
 
 -- Move between opened buffers and close them if i want
-vim.keymap.set("n", "--", ":bN<CR>")
-vim.keymap.set("n", "..", ":bp<CR>")
-vim.keymap.set("n", ",,", ":bp | bd#<CR>") -- close the buffer even if it has somiething in it
-
--- Delete shit inside ", ', }, ]
-vim.keymap.set("n", "<A-w>11", "di\"") -- Delete shit inside double quote
-vim.keymap.set("n", "<A-w>22", "di'") -- Delete shit inside single quote
-vim.keymap.set("n", "<A-w>33", "diB") -- Delete Shit inside { here }
-vim.keymap.set("n", "<A-w>44", "dib") -- Delete Shit inside ( here )
-vim.keymap.set("n", "<A-w>55", "di]") -- Delete Shit inside [ here ]
+vim.keymap.set("n", "<A-n>", ":bN<CR>")
+vim.keymap.set("n", "<A-p>", ":bp<CR>")
+vim.keymap.set("n", "<leader>cb", ":bp | bd#<CR>") -- close the buffer even if it has somiething in it

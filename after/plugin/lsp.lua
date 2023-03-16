735d86548a1c30d316ea82ef5625b2d54feb4e30
Leonardo Esparis <ljesparis@gmail.com>
2023-03-16 13:37:19 +0100

    update keymaps and lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,31 +1,29 @@
 local importer = require("ljesparis.utils")
-local lsp = importer.require('lsp-zero')
 
-lsp.preset('recommended')
+local lsp = importer.require('lsp-zero')
+local rust_tools = importer.require("rust-tools");
+
+
+-- Right now i want to have everything configured,
+-- so i'm going to use default lsp-zero preset.
+lsp.preset('recommended');
 lsp.ensure_installed({
     'rust_analyzer',
     'tsserver',
-    'eslint'
-})
+    'eslint',
+});
 
-lsp.nvim_workspace() -- configure lua for neovim
 
--- don't initialize this language server
--- we will use rust-tools to setup rust_analyzer
-lsp.skip_server_setup({ 'rust_analyzer' })
-lsp.setup()
-vim.diagnostic.config({
-    virtual_text = true,
-})
+-- Configure lua for neovim
+lsp.nvim_workspace();
 
--- Configure LSP through rust-tools.nvim plugin.
--- rust-tools will configure and enable certain LSP features for us.
--- See https://github.com/simrat39/rust-tools.nvim#configuration
+-- This option is used to let rust-tools do its job
+lsp.skip_server_setup({ 'rust_analyzer' });
+lsp.setup();
+
+-- setup rust
 local opts = {
     tools = {
-        runnables = {
-            use_telescope = true,
-        },
         inlay_hints = {
             auto = true,
             show_parameter_hints = false,
@@ -38,50 +36,50 @@ local opts = {
     -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
     server = lsp.build_options('rust_analyzer', {
         single_file_support = false,
-        cargo = {
-            buildScripts = {
-                enable = true,
-            },
-        },
-        on_attach = function(_, _)
-            -- attach something with an specific buffer
-        end
     }),
 }
-require("rust-tools").setup(opts)
+rust_tools.setup(opts);
+
+
+lsp.on_attach(function (_, bufnr)
+    local buffer_opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, buffer_opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, buffer_opts)
+    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, buffer_opts)
+    vim.keymap.set("n", "<leader>od", vim.diagnostic.open_float, buffer_opts)
+
+    -- go to next error, info or whatever
+    vim.keymap.set("n", "<C-n>", vim.diagnostic.goto_next, buffer_opts)
+
+    -- go to prev error, info or whatever
+    vim.keymap.set("n", "<C-p>", vim.diagnostic.goto_prev, buffer_opts)
+
+    -- execute a code action 
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, buffer_opts)
+
+    -- Check variable, function, classes references
+    vim.keymap.set("n", "<leader>cr", vim.lsp.buf.references, buffer_opts)
+
+    -- rename variable, function, classes references
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, buffer_opts) -- rename function, variable or whatever
+    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, buffer_opts)
+end);
+
 
 -- Setup Completion
 -- See https://github.com/hrsh7th/nvim-cmp#basic-configuration
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-})
+-- local cmp = require('cmp')
+-- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+-- local cmp_mappings = lsp.defaults.cmp_mappings({
+--    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+--    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+--    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+--    ["<C-Space>"] = cmp.mapping.complete(),
+-- })
 
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings,
-})
+-- lsp.setup_nvim_cmp({
+--    mapping = cmp_mappings,
+-- })
 
-lsp.on_attach(function(_, bufnr)
-    local buffer_opts = { buffer = bufnr, remap = false }
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, buffer_opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, buffer_opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, buffer_opts)
-    vim.keymap.set("n", "<leader>od", function() vim.diagnostic.open_float() end, buffer_opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, buffer_opts) -- go to next error, info or whatever
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, buffer_opts) -- go to prev error, info or whatever
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, buffer_opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, buffer_opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, buffer_opts) -- rename function, variable or whatever
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, buffer_opts)
-end)
 
--- auto command that will format rust files
-vim.api.nvim_create_autocmd("BufWritePre", {
-    callback = function()
-        vim.lsp.buf.format(nil, 100)
-    end,
-})
+
--- lua/ljesparis/keymaps.lua
+++ lua/ljesparis/keymaps.lua
@@ -5,16 +5,11 @@ vim.keymap.set("n", "<leader>w", ":w!<CR>")
 vim.keymap.set("v", "T", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "B", ":m '<-2<CR>gv=gv")
 
--- split vertically and split horizontally
--- navigate and close.
-vim.keymap.set("n", "spv", "<C-w>v<CR>")
-vim.keymap.set("n", "sph", "<C-w>s<CR>")
-vim.keymap.set("n", "mtl", "<C-w>h<CR>")
-vim.keymap.set("n", "mtr", "<C-w>l<CR>")
-vim.keymap.set("n", "mtt", "<C-w>k<CR>")
-vim.keymap.set("n", "mtb", "<C-w>j<CR>")
-vim.keymap.set("n", "<leader>1", "<C-w>o<CR>")
-
 -- Move between opened buffers and close them if i want
 vim.keymap.set("n", "<A-n>", ":bN<CR>")
-vim.keymap.set("n", "<leader>c", ":bp | bd#<CR>") -- close the buffer even if it has somiething in it
+
+-- close the buffer even if it has somiething in it
+vim.keymap.set("n", "<leader>c", ":bp | bd#<CR>")
+
+-- format code 
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)

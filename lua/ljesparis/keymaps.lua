b2b6757da9225b90e67c71187e1e6e8e96450be3
Leonardo Esparis <ljesparis@gmail.com>
2023-03-16 13:39:23 +0100

    change comments

--- lua/ljesparis/keymaps.lua
+++ lua/ljesparis/keymaps.lua
@@ -1,14 +1,14 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", ":w!<CR>")
 
--- Move code down and up respectively
+-- Move block of code down and up respectively
 vim.keymap.set("v", "T", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "B", ":m '<-2<CR>gv=gv")
 
--- Move between opened buffers and close them if i want
+-- Move between buffers 
 vim.keymap.set("n", "<A-n>", ":bN<CR>")
 
--- close the buffer even if it has somiething in it
+-- close the buffer even if it has something in it
 vim.keymap.set("n", "<leader>c", ":bp | bd#<CR>")
 
 -- format code 

4049a907b775da902bfa94a48f165ce8ba2776b4
Esparis <ljesparis@gmail.com>
2025-10-11 18:46:39 +0200

    configure gitsigns to have blame

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,4 +1,6 @@
 require("gitsigns").setup({
+	current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+	current_line_blame = true,
 	signs = {
 		add = { text = "+" },
 		change = { text = "~" },
@@ -6,4 +8,11 @@ require("gitsigns").setup({
 		topdelete = { text = "‾" },
 		changedelete = { text = "~" },
 	},
+
+	on_attach = function(bfnr)
+		local gitsigns = require("gitsigns")
+		vim.keymap.set("n", "gb", function()
+			gitsigns.blame_line({ full = true })
+		end, { buffer = bfnr })
+	end,
 })
